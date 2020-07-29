862190f66219a997c71efd58627d2f44d6c026cf
unknown <libibabu90@gmail.com>
2020-07-29 18:18:50 +0530

    july29 ,2020 saved all

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Array1Component } from './array1/array1.component';
+import { SimplecalcComponent } from './simplecalc/simplecalc.component';
+import { ProductentryComponent } from './productentry/productentry.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path:"home",component:Array1Component},
+                         {path:"test",component:SimplecalcComponent},
+                         {path:"pdetails",component:ProductentryComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
